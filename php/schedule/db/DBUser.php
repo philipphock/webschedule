29ec35e2a7e3199019e56729181f0373837c0362
Philipp <mail@philipphock.de>
2012-04-08 16:50:26 +0200

    salt pw auth

--- php/schedule/db/DBUser.php
+++ php/schedule/db/DBUser.php
@@ -8,23 +8,27 @@ class DBUser extends DB{
 	public function auth($username,$password){
 		$db = $this->getDB();
 		
-	    $sql = 'SELECT id, user, role FROM ' . $this->userTable . ' WHERE user = ? AND pw = ?';
+	    $sql = 'SELECT id, user, pw, role FROM ' . $this->userTable . ' WHERE user = ?';
 	    
 	    $pstmt = $db->prepare( $sql );
 	    
-		$pstmt->bind_param('ss', $username, $password);
+		$pstmt->bind_param('s', $username);
 		
 	    $pstmt->execute();
-	    $pstmt->bind_result( $id, $usr, $role );
+	    $pstmt->bind_result( $id, $usr, $pw, $role );
 	    
 	    $pstmt->fetch();
 		
-		if ($id == 0){
-			return User::createGuest();
-		}else{
-			return new User($id,$usr,$role);
+		
+		
+		if ($id != 0){
+			$salt = substr($pw, 0, 6);
+			$saltedInputPW = md5($salt . $password);
+			
+			if ($salt . $saltedInputPW == $pw)
+				return new User($id,$usr,$role);
 		}
-	
+		return User::createGuest();
 	}
 	
 }
