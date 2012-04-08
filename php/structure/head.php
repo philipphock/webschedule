f40f9f0230f984dbf4c4a4f4908afc3b30312776
Philipp <mail@philipphock.de>
2012-04-08 20:25:11 +0200

    search delete

--- php/structure/head.php
+++ php/structure/head.php
@@ -1,5 +1,7 @@
-<h1><a href="home.html">Schedule</a></h1><a href="login.html?logout=true">logout</a>
+<h1><a href="home.html">Schedule</a></h1>
+
 <form action="searchResults.html" class="fright searchfield">
+	<a id="logout_link" href="login.html?logout=true">logout</a><br>
 	<input type="text" value="search" name="q" class="searchfield"/>
 	<input type="submit" value="search" name="search" class="searchfield"/>
 </form>
